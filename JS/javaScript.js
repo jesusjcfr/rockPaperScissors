3b4e6a689f770628b5bc87a2424de0512aa72d79
Jesus <jesus.jcfr40@gmail.com>
2024-03-12 00:29:05 +0100

    -add function to play a round -get the player's choice and pass the string to lowercase

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -1,13 +1,42 @@
 //function to get computer choice
-function getComputerChoice(){
+function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
-    if(choice === 1){
-        return 'Rock'
-    } else if(choice === 2){
-        return 'Paper'
-    } else{
-        return 'Scissor'
+    if (choice === 1) {
+        return 'rock'
+    } else if (choice === 2) {
+        return 'paper'
+    } else {
+        return 'scissor'
     }
 }
 
-console.log(getComputerChoice());
+//function to declare a winner, loser or tie
+function playRound(computerSelection, playerSelection) {
+    //if to validate the player's selection
+    if(playerSelection !== 'rock' || 'paper' || 'scissor'){
+        return 'Invalid option, please select "rock, paper or scissor"';
+    }
+    //if to select a winner, loser or tie
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose!! Paper beats Rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return 'You Win!! Rock beats Scissor'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win!! Paper beats Rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose!! Scissor beats Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win!! Scissor beats Paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose!! Rock beats Scissor';
+    }else{
+        return 'Tie'
+    }
+}
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt('rock, paper or scissor?').toLowerCase();
+
+    let win = playRound(computerSelection, playerSelection);
+    console.log(win);
+
